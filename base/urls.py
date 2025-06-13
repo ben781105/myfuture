9747f87104e0e051bc69eb7c02a88315a85127de
ssewabuga <benardleno78@gmail.com>
2025-06-13 17:24:23 +0300

    second

--- base/urls.py
+++ base/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-      path('', views.login_user , name= 'login' ),
-   path('home/', views.home_view ,name= 'home'),
+   path('login/', views.login_user , name= 'login' ),
+   path('', views.home_view ,name= 'home'),
    path('members/',views.members_view, name='member'),
    path('deposits/',views.deposits_view, name='deposit'),
    path('deposit_history/',views.deposit_history,name='history'),
